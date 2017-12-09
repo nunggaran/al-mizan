f695c16d9cbeca0876fa41a2906de813e903ce0e
muhammadyana <muhammadyanaa@gmail.com>
2017-12-09 15:13:47 +0700

    Validate username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 4, maximum: 20 },format: { without: /\s/ }
   devise :invitable, :database_authenticatable, :registerable,
        	:recoverable, :rememberable, :validatable, :confirmable, :trackable, :invite_for => 2.weeks
 end
