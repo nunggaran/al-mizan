24f842b7a579720c77b585599a87e6f743eb478c
muhammadyana <muhammadyanaa@gmail.com>
2017-12-12 10:54:19 +0700

    generatae annotate

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :text
+#  slug       :string
+#  user_id    :integer
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Article < ApplicationRecord
 	#validation
 	validates :title, :content, :image, presence: true
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string
+#  content    :text
+#  slug       :string
+#  user_id    :integer
+#  image      :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
