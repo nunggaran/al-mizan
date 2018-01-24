6ad73f3b3194ddc55ae30b698869404c9f1733c2
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-01-24 13:39:14 +0700

    update

--- config/deploy.rb
+++ config/deploy.rb
@@ -57,15 +57,15 @@ namespace :deploy do
   #   run "cd #{deploy_to}/current; /usr/bin/env bundle exec rake db:seed RAILS_ENV=#{rails_env}"
   # end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      invoke 'delayed_job:restart'
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     invoke 'delayed_job:restart'
+  #     # Here we can do anything such as:
+  #     # within release_path do
+  #     #   execute :rake, 'cache:clear'
+  #     # end
+  #   end
+  # end
 
   # after 'deploy:publishing', 'deploy:restart'
   # namespace :deploy do
