52bee1da51560588606b9394b1303c341d26d8b8
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-01-25 10:12:03 +0700

    remove unused file

--- config/deploy.rb
+++ config/deploy.rb
@@ -122,13 +122,13 @@ namespace :deploy do
 #     end
 #   end
 
-#   desc 'Initial Deploy'
-#   task :initial do
-#     on roles(:app) do
-#       before 'deploy:restart', 'puma:start'
-#       invoke 'deploy'
-#     end
-#   end
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
 
 #   desc 'Restart application'
 #   task :restart do
