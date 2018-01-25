4b845cd306ab7eb4a1630e3ba471ace04e5221c2
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-01-25 12:52:44 +0700

    change setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.force_ssl = false
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
@@ -122,7 +122,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = {:host => "https://al-mizan.herokuapp.com/"}
+  config.action_mailer.default_url_options = {:host => "http://al-mizan.sch.id/"}
 end
-Rails.application.routes.default_url_options[:host] = 'https://al-mizan.herokuapp.com/'
+Rails.application.routes.default_url_options[:host] = 'http://al-mizan.sch.id/'
 
