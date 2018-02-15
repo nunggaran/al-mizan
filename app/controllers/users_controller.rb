4150b8ba2e56390a9ef1cab7bc95b21b695f72ff
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-02-15 13:35:23 +0700

    Add authenticate in alumni

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   before_action :require_same_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:edit, :update]
+  before_action :authenticate_user!
   impressionist
 
   # GET /users
